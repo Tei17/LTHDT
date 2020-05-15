f3160446e33c3fe76ad7f034d8840bdf8f075627
Tein17 <lehoang1239@gmail.com>
2020-05-15 15:56:28 +0700

    Add BaiToanDoiTien

--- BaiToanDoiTien/BaiToanDoiTien/Program.cs
+++ BaiToanDoiTien/BaiToanDoiTien/Program.cs
@@ -11,20 +11,32 @@ namespace BaiToanDoiTien
 
 		static void BaiToanDongXu()
 		{
-			int xu25, xu20, xu10, xu5, xu1;
-			Console.WriteLine("Nhập giá trị đồng xu: ");
+			Console.WriteLine("Nhập giá trị đồng xu cần quy đổi: ");
 			int n = int.Parse(Console.ReadLine());
-			xu25 = n / 25; n -= 25 * xu25;
-			xu20 = n / 20; n -= 20 * xu20;
-			xu10 = n / 10; n -= 10 * xu10;
-			xu5 = n / 5; n -= 5 * xu5;
-			Console.WriteLine("Số đồng xu các loại là: ");
-			Console.WriteLine("Loại 25 xu: {0}", xu25);
-			Console.WriteLine("Loại 20 xu: {0}", xu20);
-			Console.WriteLine("Loại 10 xu: {0}", xu10);
-			Console.WriteLine("Loại 5 xu: {0}", xu5);
-			Console.WriteLine("Loại 1 xu: {0}", n);
-			Console.WriteLine("Tổng số tờ các loại là: {0}", xu25 + xu20 + xu10 + xu5 + n);
+			Console.WriteLine("Nhập số lượng đồng xu quy đổi: ");
+			int sL = int.Parse(Console.ReadLine());
+			int[] mangLoaiTien = new int[sL];
+			int[] mangSL = new int[sL];
+			for (int i = 0; i < mangLoaiTien.Length; i++)
+			{
+				Console.WriteLine("Nhập loại tiền theo thứ tự giảm dần: ");
+				int loaiTien = int.Parse(Console.ReadLine());
+				int soLuong;
+				mangLoaiTien[i] = loaiTien;
+				soLuong = n / loaiTien;
+				n -= loaiTien * soLuong;
+				mangSL[i] = soLuong;
+			}
+			for (int j = 0; j < mangLoaiTien.Length; j++)
+			{
+				Console.WriteLine("Loại {0} là: {1}", mangLoaiTien[j], mangSL[j]);
+			}
+			int sum = 0;
+			foreach (int x in mangSL)
+			{
+				sum += x;
+			}
+			Console.WriteLine("Tổng số tờ các loại là: {0} ", sum);
 		}
 		static void Main(string[] args)
 		{
